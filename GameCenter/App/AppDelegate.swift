3da2b09b880d4a9157e1ff15dce7b2ab78ad3b93
Irsyad Ashari <irsyad.ashari17@gmail.com>
2021-01-13 15:45:00 +0800

    adding finish adding subfolders in the main app module

--- GameCenter/App/AppDelegate.swift
+++ GameCenter/App/AppDelegate.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var realm: Realm!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        realm = try! Realm()
         return true
     }
 
