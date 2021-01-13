19a17f244f13ee4c1d777a8c4564bc94311f4af8
Irsyad Ashari <irsyad.ashari17@gmail.com>
2021-01-13 16:21:04 +0800

    finishing injection class

--- GameCenter/Core/DI/Injection.swift
+++ GameCenter/Core/DI/Injection.swift
@@ -45,6 +45,65 @@ final class Injection: NSObject {
         return Interactor(repository: repository) as! U
     }
     
+    func provideSearch<U: UseCase>() -> U where U.Request == String, U.Response == [GameModel] {
+        
+        let remote = GetGamesRemoteDataSource(endpoint: Endpoints.Gets.genres.url)
+        
+        let tagMapper = TagTransformer()
+        let gameMapper = GameTransformer(tagMapper: tagMapper)
+        let mapper = GamesTransformer(gameMapper: gameMapper)
+        
+        let repository = SearchGamesRepository(remoteDataSource: remote, mapper: mapper)
+        
+        return Interactor(repository: repository) as! U
+    }
     
+    func provideGame<U: UseCase>() -> U where U.Request == String, U.Response == GameModel {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let locale = GetGamesLocaleDataSource(realm: appDelegate.realm)
+        let remote = GetGameRemoteDataSource(endpoint: Endpoints.Gets.game.url)
+        
+        let tagMapper = TagTransformer()
+        let mapper = GameTransformer(tagMapper: tagMapper)
+       
+        let repository = GetGameRepository(
+            localeDataSource: locale,
+            remoteDataSource: remote,
+            mapper: mapper)
+        
+        return Interactor(repository: repository) as! U
+    }
+    
+    func provideUpdateFavorite<U: UseCase>() -> U where U.Request == String, U.Response == GameModel {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let locale = GetFavoriteGamesLocaleDataSource(realm: appDelegate.realm)
+        
+        let tagMapper = TagTransformer()
+        let mapper = GameTransformer(tagMapper: tagMapper)
+        
+        let repository = UpdateFavoriteGameRepository(
+            localeDataSource: locale,
+            mapper: mapper)
+        
+        return Interactor(repository: repository) as! U
+    }
+    
+    func provideFavorite<U: UseCase>() -> U where U.Request == String, U.Response == [GameModel] {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let locale = GetFavoriteGamesLocaleDataSource(realm: appDelegate.realm)
+        
+        let tagMapper = TagTransformer()
+        let gameMapper = GameTransformer(tagMapper: tagMapper)
+        let mapper = GamesTransformer(gameMapper: gameMapper)
+        
+        let repository = GetFavoriteGamesRepository(
+            localeDataSource: locale,
+            mapper: mapper)
+        
+        return Interactor(repository: repository) as! U
+    }
     
 }
