61a2cc3d283f55bbd5f0c0406189e340b827bb7c
Irsyad Ashari <irsyad.ashari17@gmail.com>
2021-01-15 15:39:27 +0800

    solving the while line problem, now about to install IQKeyboardManager libs

--- GameCenter/Module/Search/View/SearchView.swift
+++ GameCenter/Module/Search/View/SearchView.swift
@@ -14,45 +14,53 @@ struct SearchView: View {
     @ObservedObject var presenter: SearchPresenter<GameModel, Interactor<String, [GameModel], SearchGamesRepository<GetGamesRemoteDataSource, GamesTransformer<GameTransformer<TagTransformer>>>>>
     
     var body: some View {
-        VStack {
+        
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
             
-            SearchBar(
-                text: $presenter.keyword,
-                onSearchButtonClicked: presenter.search
-            ).foregroundColor(.black)
-            
-            ZStack {
+            VStack {
                 
-                Color.black
-                    .edgesIgnoringSafeArea(.all)
+                SearchBar(
+                    text: $presenter.keyword,
+                    onSearchButtonClicked: presenter.search
+                ).foregroundColor(.black)
                 
-                if presenter.isLoading {
-                    loadingIndicator
-                } else if presenter.keyword.isEmpty {
-                    emptyTitle
-                } else if presenter.list.isEmpty {
-                    emptyGames
-                } else if presenter.isError {
-                    errorIndicator
-                } else {
-                    ScrollView(.vertical, showsIndicators: false) {
-                        ForEach(
-                            self.presenter.list,
-                            id: \.id
-                        ) { game in
-                            ZStack {
-                                self.linkBuilder(for: game) {
-                                    SearchRow(game: game)
-                                }.buttonStyle(PlainButtonStyle())
-                            }.padding(8)
+                Spacer()
+                
+                ZStack {
+                    
+                    if presenter.isLoading {
+                        loadingIndicator
+                    } else if presenter.keyword.isEmpty {
+                        emptyTitle
+                    } else if presenter.list.isEmpty {
+                        emptyGames
+                    } else if presenter.isError {
+                        errorIndicator
+                    } else {
+                        ScrollView(.vertical, showsIndicators: false) {
+                            ForEach(
+                                self.presenter.list,
+                                id: \.id
+                            ) { game in
+                                ZStack {
+                                    self.linkBuilder(for: game) {
+                                        SearchRow(game: game)
+                                    }.buttonStyle(PlainButtonStyle())
+                                }.padding(8)
+                            }
                         }
                     }
                 }
-            }
-        }.navigationBarTitle(
-            Text("Search Games"),
-            displayMode: .automatic
-        )
+                
+                Spacer()
+                
+            }.navigationBarTitle(
+                Text("Search Games"),
+                displayMode: .automatic
+            )
+        }
     }
 }
 
@@ -69,20 +77,20 @@ extension SearchView {
         CustomEmptyView(
             image: "Not Found",
             title: presenter.errorMessage
-        ).offset(y: 80)
+        )
     }
     
     var emptyTitle: some View {
         CustomEmptyView(
             image: "No Favorite",
             title: "Come on, find your game lads!"
-        ).offset(y: 50)
+        )
     }
     var emptyGames: some View {
         CustomEmptyView(
             image: "Not Found",
             title: "Data not found"
-        ).offset(y: 80)
+        )
     }
     
     func linkBuilder<Content: View>(
--- modules/Game/Sources/Game/Data/GetGameRepository.swift
+++ modules/Game/Sources/Game/Data/GetGameRepository.swift
@@ -44,7 +44,7 @@ where
         
         return _localeDataSource.get(id: Int(request) ?? 0)
             .flatMap { result -> AnyPublisher<GameModel, Error> in
-                if result.desc == "Unknown" {
+                if result.desc == "Unknown" || result.genre == "Unknown" {
                     return _remoteDataSource.execute(request: request)
                         .map { _mapper.transformResponseToEntity(request: request, response: $0) }
                         .catch { _ in _localeDataSource.get(id: Int(request) ?? 0) }
